f0d173bc96ae46592f182c7635e5f0451be93b93
Jahid Iqbal <jahid@simecsystem.com>
2025-01-15 17:04:41 +0600

    Initial commit

---
+++ src/main/java/org/ecom/vpecom/VanplazaEcomApplication.java
@@ -0,0 +1,13 @@
+package org.ecom.vpecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VanplazaEcomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VanplazaEcomApplication.class, args);
+    }
+
+}
